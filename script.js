f0ba63373789a801d8535d214975cb21e4b1e8e8
Rafly Hanggaraksa <ajarafly422@gmail.com>
2023-07-03 20:28:37 +0700

    finish project

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-function createBoard(size = 16) {
+function randHex () {
+    let hasil = Math.floor(Math.random()*256);
+    return hasil;
+}
+
+
+
+function changeColor(color = `rgb(${randHex()},${randHex()},${randHex()})`) {
+    const divs = document.querySelectorAll(".board div")
+    console.log("Tes")
+    divs.forEach((div)=>{
+        div.addEventListener('mouseover',()=>{
+            div.style.backgroundColor = color;
+        });
+    })
+}
+function sparkles() {
+    const divs = document.querySelectorAll(".board div")
+    console.log("Tes")
+    divs.forEach((div)=>{
+        div.addEventListener('mouseover',()=>{
+            div.style.backgroundColor = `rgb(${randHex()},${randHex()},${randHex()})`;
+        });
+    })
+}
+
+function createBoard(size = 16, color = "black") {
+    // const colorButton = document.querySelector(".control-panel button#color")
     const board = document.querySelector('.board');
-    
-    
+
     // Adding grid into the board
     board.style.gridTemplateColumns = `repeat(${size},1fr)`
     board.style.gridTemplateRows = `repeat(${size},1fr)`
@@ -14,17 +40,64 @@ function createBoard(size = 16) {
         
         // Adding Hover Event Listener
         div.addEventListener('mouseover',()=>{
-            div.style.backgroundColor = "black";
+            div.style.backgroundColor = color;
         });
         
         board.insertAdjacentElement("beforeend",div);
     }
+}
 
+function resetBoard(){
+    const divs = document.querySelectorAll(".board div")
+    console.log("Tes")
+    divs.forEach((div)=>{
+        div.style.backgroundColor="var(--bgPrimary)";
+    })
+    
+}
 
+function getSize () {
+    let size = parseInt(prompt("Choose you size between 1-100"));
+    let warn = (document.querySelector(".header p"));
+    if (size <= 0) {
+        warn.textContent = "Your value is less than 0.";
+        return false;
+    }
+    else if (size >= 100) {
+        warn.textContent = "Your value is more than 100.";
+        return false;
+    }
+    else {
+        return size;
+    }
 }
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    const buttonSize = document.querySelector(".header button");
+    const resetButton = document.querySelector(".control-panel button#reset")
+    const colorButton = document.querySelector(".control-panel button#color")
+    const sparklesButton = document.querySelector(".control-panel button#sparkles")
+    let size = 16;
     createBoard();
+
+    buttonSize.addEventListener('click',()=>{
+        size = getSize();
+        if (size === false) return;
+        createBoard(size);
+        resetBoard();
+    });
+
+    resetButton.addEventListener('click',()=>{
+        resetBoard();
+    });
+
+    colorButton.addEventListener('click', () => {
+        changeColor();
+    })
+    sparklesButton.addEventListener('click', () => {
+        sparkles();
+    })
+
 })
